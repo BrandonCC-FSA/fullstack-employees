a5fb4f8c681711b9034ab49495c0eb5833980b44
Brandon Corrigall <corrb812@gmail.com>
2025-06-25 15:00:45 -0400

    installed faker and used to seed fake employees into the newly created fullstack_employees database, as laidout by schema.sql, and perfomed by employees.js function createEmployee

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
+import { createEmployee } from "./queries/employees.js";
 
 await db.connect();
 await seedEmployees();
@@ -7,4 +9,96 @@ console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
   // TODO
+  console.log("CREATING EMPLOYEES");
+  await createEmployee({
+    name: faker.person.fullName(),
+    birthday: faker.date.birthdate({ mode: "age", min: 18, max: 65 }),
+    salary: faker.finance.amount({
+      min: 50000,
+      max: 100000,
+      dec: 0,
+    }),
+  });
+  await createEmployee({
+    name: faker.person.fullName(),
+    birthday: faker.date.birthdate({ mode: "age", min: 18, max: 65 }),
+    salary: faker.finance.amount({
+      min: 50000,
+      max: 100000,
+      dec: 0,
+    }),
+  });
+  await createEmployee({
+    name: faker.person.fullName(),
+    birthday: faker.date.birthdate({ mode: "age", min: 18, max: 65 }),
+    salary: faker.finance.amount({
+      min: 50000,
+      max: 100000,
+      dec: 0,
+    }),
+  });
+  await createEmployee({
+    name: faker.person.fullName(),
+    birthday: faker.date.birthdate({ mode: "age", min: 18, max: 65 }),
+    salary: faker.finance.amount({
+      min: 50000,
+      max: 100000,
+      dec: 0,
+    }),
+  });
+  await createEmployee({
+    name: faker.person.fullName(),
+    birthday: faker.date.birthdate({ mode: "age", min: 18, max: 65 }),
+    salary: faker.finance.amount({
+      min: 50000,
+      max: 100000,
+      dec: 0,
+    }),
+  });
+  await createEmployee({
+    name: faker.person.fullName(),
+    birthday: faker.date.birthdate({ mode: "age", min: 18, max: 65 }),
+    salary: faker.finance.amount({
+      min: 50000,
+      max: 100000,
+      dec: 0,
+    }),
+  });
+  await createEmployee({
+    name: faker.person.fullName(),
+    birthday: faker.date.birthdate({ mode: "age", min: 18, max: 65 }),
+    salary: faker.finance.amount({
+      min: 50000,
+      max: 100000,
+      dec: 0,
+    }),
+  });
+  await createEmployee({
+    name: faker.person.fullName(),
+    birthday: faker.date.birthdate({ mode: "age", min: 18, max: 65 }),
+    salary: faker.finance.amount({
+      min: 50000,
+      max: 100000,
+      dec: 0,
+    }),
+  });
+  await createEmployee({
+    name: faker.person.fullName(),
+    birthday: faker.date.birthdate({ mode: "age", min: 18, max: 65 }),
+    salary: faker.finance.amount({
+      min: 50000,
+      max: 100000,
+      dec: 0,
+    }),
+  });
+  await createEmployee({
+    name: faker.person.fullName(),
+    birthday: faker.date.birthdate({ mode: "age", min: 18, max: 65 }),
+    salary: faker.finance.amount({
+      min: 50000,
+      max: 100000,
+      dec: 0,
+    }),
+  });
+  console.log("EMPLOYEES CREATED");
 }
